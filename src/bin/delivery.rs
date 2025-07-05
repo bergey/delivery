23d9e9427dcad23a945fd2c297a85959243ccac2
Daniel Bergey <daniel.bergey@braze.com>
2025-07-05 06:55:22 -0400

    axum hello world

--- src/bin/delivery.rs
+++ src/bin/delivery.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/menu", get(menu));
+    // let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn menu() -> &'static str{
+    "Hello, world!"
 }
